1e6685f8f8dfddf4380e810604e637f1a6e38d47
osdever <krasota156@gmail.com>
2020-04-23 19:20:05 +0300

    Added an #ifdef

--- CXIfaces/include/CXFramework/Interface.h
+++ CXIfaces/include/CXFramework/Interface.h
@@ -9,10 +9,13 @@ namespace CX
 	{
 	private:
 		static char _HandleDummy;
-	public:
 		static constexpr IfaceHandle CXGetIHandle() {
 			return &_HandleDummy;
 		}
+
+#ifdef CXCFG_HIDE_IHANDLES
+		friend class Interfaceable;
+#endif
 	};
 	template<class I>
 	char Interface<I>::_HandleDummy = 0;
