301fae326400e2a930ce856ffcaba72e5a1f822f
osdever <krasota156@gmail.com>
2020-04-24 10:53:13 +0300

    Registry reading

--- CXRefl_Recreation/src/CXReflRC_Main.cpp
+++ CXRefl_Recreation/src/CXReflRC_Main.cpp
@@ -35,7 +35,7 @@ namespace Registry
 		RegSetValueEx(hk, name.c_str(), 0, type, (BYTE*)acc.data(), (DWORD) acc.size());
 	}
 
-	void Write(HKEY hRootKey, const _StringType& path, CX::Reflection::Reflected& val)
+	void Write(HKEY hRootKey, const String& path, CX::Reflection::Reflected& val)
 	{
 		HKEY hk;
 		RegCreateKeyEx(hRootKey, path.c_str(), 0, nullptr, 0, KEY_ALL_ACCESS, nullptr, &hk, nullptr);
@@ -48,7 +48,7 @@ namespace Registry
 			else if (p.is<String>())
 			{
 				auto s = p.as<String>();
-				RegSetValueEx(hk, STConvert(p.name()).c_str(), 0, REG_SZ, (BYTE*)s.c_str(), (DWORD) s.length() * sizeof(String::value_type));
+				RegSetValueEx(hk, STConvert(p.name()).c_str(), 0, REG_SZ, (BYTE*)s.c_str(), (DWORD)s.length() * sizeof(String::value_type));
 			}
 			else if (p.is<MultiString>())
 			{
@@ -57,6 +57,33 @@ namespace Registry
 			else
 			{
 				SetRegValue_Simple(hk, STConvert(p.name()), REG_BINARY, p);
+			}
+		}
+		RegCloseKey(hk);
+	}
+	void Read(HKEY hRootKey, const String& path, CX::Reflection::Reflected& val)
+	{
+		HKEY hk;
+		RegCreateKeyEx(hRootKey, path.c_str(), 0, nullptr, 0, KEY_ALL_ACCESS, nullptr, &hk, nullptr);
+		for (auto& p : val.properties())
+		{
+			std::printf("Read: %s\n", p.name().c_str());
+			if (p.is<String>())
+			{
+				DWORD readSize;
+				RegQueryValueEx(hk, STConvert(p.name()).c_str(), nullptr, nullptr, nullptr, &readSize);
+
+				String s;
+				s.reserve(readSize);
+				RegQueryValueEx(hk, STConvert(p.name()).c_str(), nullptr, nullptr, (BYTE*)s.data(), &readSize);
+
+				p.as<String>() = std::move(s);
+			}
+			else
+			{
+				size_t expectedSize = p.size();
+				DWORD readSize;
+				RegQueryValueEx(hk, STConvert(p.name()).c_str(), nullptr, nullptr, (BYTE*)p.data(), &readSize);
 			}
 		}
 		RegCloseKey(hk);
@@ -77,6 +104,21 @@ int main() {
 	s.HumanPopulation = 7800000000;
 	s.PopupCount = 20;
 	s.SkynetNickname = Registry::STConvert("Dz3n");
+	Registry::Read(HKEY_CURRENT_USER, Registry::STConvert("Software\\SkynetCorporation"), s);
+	
+	for (auto& p : s.properties())
+	{
+		std::printf("%s: ", p.name().c_str());
 
-	Registry::Write(HKEY_CURRENT_USER, Registry::STConvert("Software\\SkynetCorporation"), s);
+		if (p.is<Registry::Dword>())
+			std::printf("0x%X", p.as<Registry::Dword>());
+		else if (p.is<Registry::Qword>())
+			std::printf("%llu", p.as<Registry::Qword>());
+		else if (p.is<Registry::Bool>())
+			std::printf("%d", p.as<Registry::Bool>());
+		else if (p.is<Registry::String>())
+			std::printf("'%s'", p.as<Registry::String>().c_str());
+
+		std::printf("\n");
+	}
 }
